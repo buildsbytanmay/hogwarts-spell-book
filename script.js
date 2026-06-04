40dd1a616f88784942b05217c47425b5a61c9714
buildsbytanmay <dushantb123@gmail.com>
2026-06-04 23:15:27 +0530

    getting spells

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 const URL = "https://potterapi-fedeperin.vercel.app/en/spells";
 // const URL = "https://potterapi-fedeperin.vercel.app/es/characters?search=Weasley";
 // const URL = "https://potterapi-fedeperin.vercel.app/en/books";
+
+async function getSpells(i){
+    let response = await fetch(URL);
+    let spells = await response.json();
+    let spellbox = document.querySelector("#box1");
+    let usebox = document.querySelector("#box2");
+    spellbox.innerText = spells[i].spell;
+    usebox.innerText = spells[i].use;
+}
