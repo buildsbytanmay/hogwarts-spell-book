708b160445e7a3947adcf4c63c7bd3ff088b8079
buildsbytanmay <dushantb123@gmail.com>
2026-06-04 23:13:40 +0530

    URL created

---
+++ script.js
@@ -0,0 +1,3 @@
+const URL = "https://potterapi-fedeperin.vercel.app/en/spells";
+// const URL = "https://potterapi-fedeperin.vercel.app/es/characters?search=Weasley";
+// const URL = "https://potterapi-fedeperin.vercel.app/en/books";
